d94876832bf0d0484e15c547ddc4212712e0e521
Aliaksei Zakharau <aliaksei.zakharau.92@gmail.com>
2022-08-22 17:03:53 +0200

    fix: Add None value bypass for source method value in cmd_parser. Add exceptions for printing methods and error_msg method.

--- rss_reader.py
+++ rss_reader.py
@@ -5,9 +5,13 @@ import argparse
 app_name = 'RSS READER'
 # Version of application
 app_version = '0.02'
+
+
 # CHANGELOG
 # 19.08.22 v.0.01 Add doc strings. Create start function.
 # 19.08.22 v.0.02 Add error_source method of rss_feed class. Add requirements.txt.
+# 22.08.22 v.0.03 fix: Add None value bypass for source method value in cmd_parser.
+#                 Add error_msg method. Add exceptions for absence keys.
 
 class cmd_parser:
     """
@@ -23,22 +27,21 @@ class cmd_parser:
         Method for parse arguments in cmd user input
         """
 
-        parser = argparse.ArgumentParser(prog = str(app_name))
+        parser = argparse.ArgumentParser(prog=str(app_name))
 
         parser.add_argument('-v', '--version', action='version',
-                    version = str(app_name + ' v.' + app_version),
-                    help="Print version info")
+                            version=str(app_name + ' v.' + app_version),
+                            help="Print version info")
         parser.add_argument('--json', action='store_true',
-                    help="Print result as JSON in stdout")
+                            help="Print result as JSON in stdout")
         parser.add_argument('--verbose', action='store_true',
-                    help="Outputs verbose status messages")
+                            help="Outputs verbose status messages")
         parser.add_argument('--limit', action='store', type=int,
-                    help="Limit news topics if this parameter provided")
-        parser.add_argument('source', action='store', type=str,
-                    help="RSS URL")
+                            help="Limit news topics if this parameter provided")
+        parser.add_argument('source',  nargs='?', action='store', type=str,
+                            help="RSS URL")
 
         args = parser.parse_args()
-
         return args
 
     def json(self):
@@ -71,8 +74,12 @@ class cmd_parser:
         (link to rss feed)
         """
         output = self.input_parser()
-        return output.source
-
+        if output.source == None:
+            err_text = "RSS READER: error: the following arguments are required: source"
+            print(err_text)
+            raise SystemExit(0)
+        else:
+            return output.source
 
 
 class rss_feed:
@@ -92,30 +99,48 @@ class rss_feed:
         """
         return feedparser.parse(self.s_link)
 
+    def error_msg(self, name):
+        """
+        Methode for print error message about absence key in feed
+        """
+        print("RSS READER: error: the following key absence in RSS feed: ", name)
+
+
     def news_source(self, item):
         """
         Method for print source of news from rss feed
         """
-        print("Source: ", item.source.title)
+        try:
+            print("Source: ", item.source.title)
+        except:
+            self.error_msg('source')
 
-    def news_title(self,item):
+    def news_title(self, item):
         """
         Method for print title of news from rss feed
         """
-        print("Title: ",item.title)
-
+        try:
+            print("Title: ", item.title)
+        except:
+            self.error_msg('title')
 
     def news_date(self, item):
         """
         Method for print publish date of news from rss feed
         """
-        print("Date: ",item.published)
+        try:
+            print("Date: ", item.published)
+        except:
+            self.error_msg('date')
 
     def news_link(self, item):
         """
         Method for print link to news from rss feed
         """
-        print("Link:", item.link)
+        try:
+            print("Link:", item.link)
+        except:
+            self.error_msg('link')
 
     def space(self):
         """
@@ -127,7 +152,7 @@ class rss_feed:
         """
         Method for print user visible message about source error
         """
-        print("Error reading RSS feed. Check the source.")
+        print("RSS READER: error: reading RSS feed. Check the source.")
 
     def print_info(self):
         """
@@ -156,6 +181,7 @@ class rss_feed:
         else:
             self.error_source()
 
+
 def start():
     """
     Main function for start and work rss reader
@@ -168,9 +194,6 @@ def start():
     else:
         news.print_json()
 
+
 if __name__ == '__main__':
     start()
-
-
-
-
